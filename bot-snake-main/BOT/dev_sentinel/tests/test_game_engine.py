ced118b403a3441b9970490d22ba03a95d683050
lucas-hm <lucashugomoran12@gmail.com>
2026-08-17 19:26:02 -0300

    fixes

--- bot-snake-main/BOT/dev_sentinel/tests/test_game_engine.py
+++ bot-snake-main/BOT/dev_sentinel/tests/test_game_engine.py
@@ -261,7 +261,7 @@ class TestGameEngineCoverage(unittest.TestCase):
         res = payload
 
         self.assertTrue(res)
-        self.assertEqual(res.metadata["strategy"], "MCTS_Combat_Tactics") # type: ignore
+        self.assertEqual(res["strategy"], "MCTS_Combat_Tactics") # type: ignore
 
     def test_mcts_returns_none_fallback_to_bfs(self):
         """Si MCTS no encuentra movimiento, cae al flujo BFS estándar."""
